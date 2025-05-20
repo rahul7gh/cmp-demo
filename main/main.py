acdcc446df6e8881a7eae6fdc0a3daf441e40996
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-05-20 12:50:27 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,11 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Process extra vars")
-parser.add_argument("--arg1", type=str, help="Argument 1")
-parser.add_argument("--arg2", type=int, help="Argument 2")
+parser.add_argument("--argument1", type=str, help="Argument 1")
+parser.add_argument("--argument2", type=int, help="Argument 2")
 
 args = parser.parse_args()
 
 print("Received parameters:")
-print(f"Arg1: {args.arg1}")
-print(f"Arg2: {args.arg2}")
+print(f"Arg1: {args.argument1}")
+print(f"Arg2: {args.argument2}")
