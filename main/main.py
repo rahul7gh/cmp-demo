b6ddbdf1de5ec3cf09e2726e8982cfadee4e9864
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-05-21 16:12:39 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -2,7 +2,7 @@ import argparse
 import json
 parser = argparse.ArgumentParser(description="Process extra vars")
 parser.add_argument("--ipaddress", type=str, help="ipaddress")
-parser.add_argument("--machine_name", type=int, help="machine_name")
+parser.add_argument("--machine_name", type=str, help="machine_name")
 
 args = parser.parse_args()
 output={"ipaddress":parser.ipaddress,"machine_name":parser.machine_name,"cpu":"2GB","memory":"16384MB","disk":"131072MB"}
