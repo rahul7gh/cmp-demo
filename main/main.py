2d0a8cc27047a6fc2befda830b756cd994b8bebf
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-05-21 16:04:48 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Process extra vars")
-parser.add_argument("--ipaddress", type=str, help="ipaddress)
+parser.add_argument("--ipaddress", type=str, help="ipaddress")
 parser.add_argument("--machine_name", type=int, help="machine_name")
 
 args = parser.parse_args()
