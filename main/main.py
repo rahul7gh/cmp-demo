0b739dcfef9c2be9d68ffe7d3b7ecf81971ab12e
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-05-21 15:50:35 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -5,5 +5,5 @@ parser.add_argument("--ipaddress", type=str, help="ipaddress)
 parser.add_argument("--machine_name", type=int, help="machine_name")
 
 args = parser.parse_args()
-output={"cpu":"2GB",memory:"16384MB",disk:"131072MB"}
+output={"cpu":"2GB","memory":"16384MB","disk":"131072MB"}
 print(json.dumps({"output":output}))
