2604efe277f46e4e464f0b08896791b3556c5678
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-05-21 10:35:40 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -7,5 +7,5 @@ parser.add_argument("--argument2", type=int, help="Argument 2")
 args = parser.parse_args()
 
 print("Received parameters:")
-print(f"Arg1: {args.argument1}")
-print(f"Arg2: {args.argument2}")
+print(f"Argument 1: {args.argument1}")
+print(f"Argument 2: {args.argument2}")
