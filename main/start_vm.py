9bbff65bef391cedca5e0272798ee231278a43ec
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-06-29 11:50:17 +0530

    Create start_vm.py

---
+++ main/start_vm.py
@@ -0,0 +1,11 @@
+import argparse
+import json
+parser = argparse.ArgumentParser(description="Process extra vars")
+parser.add_argument("--ipaddress", type=str, help="ipaddress")
+
+args = parser.parse_args()
+output={"ipaddress":args.ipaddress,"status":"ON"}
+print(json.dumps({"output":output}))
+
+
+#python3 test.py --ipaddress=10.25.10.20 --machine_name=YOSH
