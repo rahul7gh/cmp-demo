af5b5c9cc3264fc97c29edc64994f0f1680801f7
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-07-15 12:15:13 +0530

    Update start_vm.py

--- main/start_vm.py
+++ main/start_vm.py
@@ -3,9 +3,12 @@ import json
 parser = argparse.ArgumentParser(description="Process extra vars")
 parser.add_argument("--ipaddress", type=str, help="ipaddress")
 parser.add_argument("--comments", type=str, help="comments")
+parser.add_argument("--instance_spec_vars", type=str, help="instance_spec_vars")
+
+loaded_data=json.laods(args.instance_spec_vars)
 
 args = parser.parse_args()
-output={"ipaddress":args.ipaddress,"status":"ON","comments":args.comments}
+output={"ipaddress":args.ipaddress,"status":"ON","comments":args.comments,"instance_spec_vars":loaded_data}
 print(json.dumps({"output":output}))
 
 
