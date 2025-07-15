f9b92b1d516a47350276d8fa8b84b3330ef091da
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-07-15 12:22:34 +0530

    Update start_vm.py

--- main/start_vm.py
+++ main/start_vm.py
@@ -5,9 +5,10 @@ parser.add_argument("--ipaddress", type=str, help="ipaddress")
 parser.add_argument("--comments", type=str, help="comments")
 parser.add_argument("--instance_spec_vars", type=str, help="instance_spec_vars")
 
+args = parser.parse_args()
+
 loaded_data=json.loads(args.instance_spec_vars)
 
-args = parser.parse_args()
 output={"ipaddress":args.ipaddress,"status":"ON","comments":args.comments,"instance_spec_vars":loaded_data}
 print(json.dumps({"output":output}))
 
